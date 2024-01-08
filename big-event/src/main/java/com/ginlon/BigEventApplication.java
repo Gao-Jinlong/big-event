25261bba8871b3531ebe41eda12a376790bb7ad3
gao-jinlong <2675130594@qq.com>
2024-01-09 00:07:13 +0800

    fix(application.yml): application.yml 默认路径为 src/main/resources

--- big-event/src/main/java/com/ginlon/BigEventApplication.java
+++ big-event/src/main/java/com/ginlon/BigEventApplication.java
@@ -2,12 +2,11 @@ package com.ginlon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Hello world!
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class BigEventApplication {
     public static void main(String[] args) {
         SpringApplication.run(BigEventApplication.class, args);
