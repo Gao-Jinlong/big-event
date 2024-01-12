189e81b399fb732f1b044bd45f1a6ce7072ff517
gao-jinlong <2675130594@qq.com>
2024-01-12 22:21:17 +0800

    feat(test)

---
+++ big-event/src/main/java/com/ginlon/controller/ArticleController.java
@@ -0,0 +1,21 @@
+package com.ginlon.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ginlon.pojo.Result;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+  @GetMapping("/list")
+  public Result<String> list() {
+    return Result.success();
+  }
+
+}
---
+++ big-event/src/test/java/JwtTest.java
@@ -0,0 +1,39 @@
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Date;
+import java.util.HashMap;
+
+public class JwtTest {
+  @Test
+  public void testGen() {
+    Map<String, Object> claims = new HashMap<>();
+    claims.put("id", "1");
+    claims.put("username", "张三");
+
+    String token = JWT.create().withClaim("user", claims)
+        .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 12))
+        .sign(Algorithm.HMAC256("ginlon"));
+
+    System.out.println(token);
+  }
+
+  @Test
+  public void testParse() {
+    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiMSIsInVzZXJuYW1lIjoi5byg5LiJIn0sImV4cCI6MTcwNTExMjA5NH0.IG7SLvpJsxNmETiUC2pw8kIII7JbC0zmknMd5qzcMWg";
+    JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256("ginlon")).build();
+
+    DecodedJWT decodedJWT = jwtVerifier.verify(token);
+
+    Map<String, Claim> claims = decodedJWT.getClaims();
+
+    System.out.println(claims.get("user"));
+  }
+}
