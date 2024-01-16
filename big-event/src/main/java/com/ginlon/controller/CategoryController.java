33097373457b2f2d4360649c6c402296c3870f6d
gao-jinlong <2675130594@qq.com>
2024-01-16 23:52:35 +0800

    feat(category):add validated

--- big-event/src/main/java/com/ginlon/controller/CategoryController.java
+++ big-event/src/main/java/com/ginlon/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ginlon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class CategoryController {
   private CategoryService categoryService;
 
   @PostMapping
-  public Result add(@RequestBody Category category) {
+  public Result add(@RequestBody @Validated Category category) {
     categoryService.add(category);
     return Result.success();
   }
--- big-event/src/main/java/com/ginlon/pojo/Category.java
+++ big-event/src/main/java/com/ginlon/pojo/Category.java
@@ -2,12 +2,15 @@ package com.ginlon.pojo;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class Category {
   private Integer id;
+  @NotEmpty(message = "分类名称不能为空")
   private String categoryName;
+  @NotEmpty(message = "分类别名不能为空")
   private String categoryAlias;
   private Integer createUser;
   private LocalDateTime createTime;
